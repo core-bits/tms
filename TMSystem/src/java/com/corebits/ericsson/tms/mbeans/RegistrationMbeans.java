e15b9518661e0d60aa37ec43f52e830014e9122a
gentletom2004 <gentletom2004@yahoo.co.uk>
2016-07-31 15:54:01 +0100

    Package Structure
    Include package structure for controller and means

---
+++ TMSystem/src/java/com/corebits/ericsson/tms/mbeans/RegistrationMbeans.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corebits.ericsson.tms.mbeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Tommy
+ */
+@Named(value = "register")
+@RequestScoped
+public class RegistrationMbeans {
+
+    /**
+     * Creates a new instance of RegistrationMbeans
+     */
+    public RegistrationMbeans() {
+    }
+    
+}

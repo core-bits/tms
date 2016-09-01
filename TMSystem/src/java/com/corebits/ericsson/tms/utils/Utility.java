dba63d4b6552fe22e24d1db84f01864e4f6d4f5b
gentletom2004 <gentletom2004@yahoo.co.uk>
2016-09-01 21:55:49 +0100

    Changes in registration and contribution
    Changes in registration and contribution

--- TMSystem/src/java/com/corebits/ericsson/tms/utils/Utility.java
+++ TMSystem/src/java/com/corebits/ericsson/tms/utils/Utility.java
@@ -10,4 +10,6 @@ public class Utility {
    public static final String OPERATION_STATUS = "OK";
    public static final String PERSISTENCE_CONTEXT_UNIT_NAME = "TMSystemPU";
     
+   public static final Short APPLICATION_STATUS_PENDING = 0;
+   public static final Short APPLICATION_STATUS_APPROVED = 1;
 }

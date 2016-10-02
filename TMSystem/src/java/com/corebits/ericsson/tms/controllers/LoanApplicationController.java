390c11aa7da15c48ad2ee014fd833b24bfc28d4f
oche-omale <oche.omale@gmail.com>
2016-10-02 23:00:36 +0200

    fix differences

--- TMSystem/src/java/com/corebits/ericsson/tms/controllers/LoanApplicationController.java
+++ TMSystem/src/java/com/corebits/ericsson/tms/controllers/LoanApplicationController.java
@@ -18,6 +18,7 @@ public class LoanApplicationController extends AbstractController<LoanApplicatio
     public static final String NAMED_QUERY_FIND_ALL = "LoanApplication.findAll";
     public static final String FIND_MEMBER_LOAN_APPLICATION = "LoanApplication.findByMemberId";
     public static final String FIND_LOAN_APPLICATION_BY_LOAN_ID = "LoanApplication.findByLoanById";
+    public static final String LOAN_APPLICATION_BY_LOAN_STATUS = "LoanApplication.findByLoanStatus";
 
     @Override
     protected EntityManager getEntityManager() {
